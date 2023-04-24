70acb7f3c0f6246fe729976dd47302adde8baaf5
Mikhail Kiselev <mkiselev1@yandex.ru>
2023-04-24 16:47:35 +0300

    receptors were silent

--- ping-pong/environment/AdaptiveSpikeSource.cpp
+++ ping-pong/environment/AdaptiveSpikeSource.cpp
@@ -4,15 +4,13 @@
 extern int ntact;
 extern RandomNumberGenerator rng;
 
-float rStateFiringFrequency /* = 5.F / NeuronTimeDepth */;
+float rStateFiringFrequency = 0.3F;
 
 bool AdaptiveSpikeSource::bFire()
 {
     if (LastTactinThisState < ntact - 1)
         rCurrentFrequency = rStateFiringFrequency;
     bool bret = rng() < rCurrentFrequency;
-//        if (bret && rCurrentFrequency > 3.F / NeuronTimeDepth)
-//            rCurrentFrequency *= 0.5F;
     LastTactinThisState = ntact;
     return bret;
 }
