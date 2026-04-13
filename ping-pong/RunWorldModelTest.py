8d2b4655f466beba046de248b39463a31e2eff1a
Mikhail Kiselev <mkiselev1@yandex.ru>
2026-04-13 18:40:34 +0300

    RunWorldModelTest.py added

---
+++ ping-pong/RunWorldModelTest.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 13 13:14:12 2026
+
+@author: mike
+"""
+
+import pandas as pd
+import random
+import subprocess
+
+filestates = "ping_pong_state_complete.csv"
+fileres = "WorldDynamicsModel.res"
+
+nTests = 3000
+
+df = pd.read_csv(filestates)
+
+with open(fileres, "w") as filres:
+    filres.write("tactstart,tactfinish,yreal,ypred\n")
+    test = 0
+    while test < nTests:
+        nr = df.shape[0]
+        indstart = random.randint(0, nr - 1)
+        if df.iat[indstart, 1] > 0:
+            indfinish = indstart + 1
+            while indfinish < nr and df.iat[indfinish, 1] > 0:
+                indfinish += 1
+            if indfinish < nr:
+                ground_truth = df.iat[indfinish, 3]
+                try:
+                    p = subprocess.Popen(["./ArNIGPU", "../Experiments", "-e20000", "-T3000"], stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
+                    outs, errs = p.communicate(str(indstart) + '\n')
+                    if p.returncode != 0 or len(errs) == 0 or errs[:4] != 'ArNI':
+                        print("Abnormal ArNIGPU termination!")
+                        exit(-1)
+                    try:
+                        res = int(errs[4:])
+                    except:
+                        print("Unexpected ArNIGPU result!")
+                        exit(-1)
+                except:
+                    print("Cannot run ArNIGPU!")
+                    exit(-1)
+                filres.write(f"{indstart},{indfinish},{ground_truth},{res}\n")
+                test += 1
+                print(test, " tests...")
+                filres.flush()
+        
+
